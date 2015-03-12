7420931e6a1725e07239332e1e32e0be8f22b2b7
opsadmin <opsadmin@stg1-deploy02.rabbit.local>
2015-03-12 16:53:47 +0900

    change how to get signature

--- cstack/compute/stack.py
+++ cstack/compute/stack.py
@@ -11,6 +11,7 @@ import os
 import simplejson
 from lxml import etree
 import httplib2
+import hashlib
 
 class Stack(object):
     def __init__(self, http, host, api_key, secret_key):
@@ -20,26 +21,22 @@ class Stack(object):
         self.secret_key = secret_key
 
     def signature(self, command, query):
-        query_params = dict(command=command)
-
+        query['command'] = command
+        query['apikey'] = self.api_key
+ 
         if not query or not query.has_key('response'):
             query['response'] = 'json'
 
-        query_params.update([q.replace('+','%20').split("=") for q in
-                             urllib.urlencode(query).split("&")])
-        quoted_query = "&".join(["%s=%s" % (k,v) for (k,v) in query_params.items()])
-
-        params = dict([(k,v.lower()) for (k,v) in query_params.items()])
-        params["apiKey".lower()] = self.api_key.lower()
-        msg = "&".join(["%s=%s" % (k,v) for (k,v) in sorted(params.items())])
-        hashed = hmac.new(self.secret_key, msg, sha)
-        return (quoted_query, urllib.quote(base64.b64encode(hashed.digest())))
+        query_str = '&'.join(['='.join([k,urllib.quote_plus(query[k])]) for k in query.keys()])
+        signature_str = '&'.join(['='.join([k.lower(),urllib.quote_plus(query[k].lower().replace('+','%20'))])for k in sorted(query.iterkeys())])
+        signature = urllib.quote_plus(base64.encodestring(hmac.new(self.secret_key,signature_str,hashlib.sha1).digest()).strip())        
+        return (query_str, signature)
 
     def url(self, command, query):
         quoted_query, signature = self.signature(command,query)
-        return "%s?%s&apiKey=%s&signature=%s" % (self.host,
-                                                 quoted_query,
-                                                 self.api_key, signature)
+        return "%s?%s&signature=%s" % (self.host,
+                                       quoted_query,
+                                       signature)
 
     def connect(self, method, command, query):
         try:
@@ -61,3 +58,4 @@ class Stack(object):
 
     def get(self, command, query=None):
         return self.connect('GET',command,query)
+
